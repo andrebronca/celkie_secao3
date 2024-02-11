6339a9918e9e78d23501237977b063b32092e98a
André R. Bronca <bronca.andre@gmail.com>
2024-02-11 10:31:52 -0300

    Configurando a ConfigController para validar se há conteúdo na url. Testando a chegada de parâmetros de query string.

--- core/ConfigController.php
+++ core/ConfigController.php
@@ -8,6 +8,21 @@ class ConfigController{
 
     public function __construct()
     {
-        echo "carregando configcontroller";
+        echo "carregando configcontroller <br>";
+        //'url' é o que foi definida no .htaccess
+        $filter = filter_input(INPUT_GET, 'url', FILTER_DEFAULT);
+        if(!empty($filter)){
+            $this->url = $filter;
+            var_dump($this->url);
+            /* testando a captura da query string
+            http://localhost/celke_secao3/artigos/index?situacao=1&origem=email
+            $situacao = filter_input(INPUT_GET, 'situacao', FILTER_DEFAULT);
+            $origem = filter_input(INPUT_GET, 'origem', FILTER_DEFAULT);
+            var_dump($situacao);
+            var_dump($origem);
+            */
+        } else {
+            echo "Acessando a página inicial <br>";
+        }
     }
 }
