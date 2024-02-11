21b93ac650a914ab890c3563d898891b6fa4a393
André R. Bronca <bronca.andre@gmail.com>
2024-02-11 10:05:42 -0300

    Iniciando o composer para criação do autoload. Criando a classe ConfigController e vinculando ao index.php. Testando

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-andrepc',
+        'version' => 'dev-andrepc',
+        'reference' => '60e8cc11b56171ec3b8bc4033806a6b2df4ae2d1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-andrepc',
+            'version' => 'dev-andrepc',
+            'reference' => '60e8cc11b56171ec3b8bc4033806a6b2df4ae2d1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
